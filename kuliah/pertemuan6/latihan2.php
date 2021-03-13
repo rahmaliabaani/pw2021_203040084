b5295fc8adbceaadb48dc06cfbc72f156ff5da98
rahmaliabaani <rahmaliarpla15@gmail.com>
2021-03-13 20:11:20 +0700

    Commit Pertemuan 6 dan Prak 3

---
+++ kuliah/pertemuan6/latihan2.php
@@ -0,0 +1,47 @@
+<?php 
+// $mahasiswa = [
+// 	["Rahma", "203040084", "rahma@gmail.com", "Teknik Informatika"],
+// 	["Bunga", "203040088", "bunga@gmail.com", "Teknik Informatika"]];
+
+// array associative
+// definisnya sama seperti array numerik kecuali
+// keynya adalah string yang kita buat sendiri
+
+$mahasiswa = [["gambar" => "2.jpg",
+			  "nama" => "Rahma",
+			  "nrp" => "203040084",
+			  "email" => "rahma@gmail.com",
+			  "jurusan" => "Teknik Informatika"],
+
+			  ["gambar" => "1.jpg",
+			  "nama" => "Rahma",
+			  "nrp" => "203040084",
+			  "email" => "rahma@gmail.com",
+			  "jurusan" => "Teknik Informatika"]];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=	, initial-scale=1.0">
+	<title>Daftar mahasiswa</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+
+	<?php foreach ($mahasiswa as $mhs) { ?>
+		<ul>
+			<li>
+				<img src="img/<?php echo $mhs["gambar"];?>" alt="">
+			</li>	
+			<li>Nama : <?php echo $mhs["nama"];?></li>	
+			<li>NRP : <?php echo $mhs["nrp"];?></li>	
+			<li>Email : <?php echo $mhs["email"];?></li>	
+			<li>Jurusan : <?php echo $mhs["jurusan"];?></li>	
+		</ul>
+	<?php } ?>
+	
+</body>
+</html>
---
+++ praktikum/tugas3/latihan3a.php
@@ -0,0 +1,4 @@
+<?php 
+$sambung = ["ada", "abel", "men", "pung", "nilai"];
+echo "Array $sambung[0]lah suatu vari$sambung[1] yang dapat $sambung[2]am$sambung[3] banyak $sambung[4]";
+?>
