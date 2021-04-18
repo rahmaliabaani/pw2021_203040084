1d94f5bda384d69308c92d2c6d037a2c99b68516
rahmaliabaani <rahmaliarpla15@gmail.com>
2021-04-18 11:58:41 +0700

    Commit prak5

--- praktikum/tugas5/latihan5c/index.php
+++ praktikum/tugas5/latihan5c/index.php
@@ -10,7 +10,7 @@ $tupperware = query("SELECT * FROM tupperware");
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Latihan 5b</title>
+	<title>Latihan 5c</title>
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!-- my css -->
